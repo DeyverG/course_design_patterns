8edf6104c50dca410d9fd398a66f122b6a3878ba
DeyverG <deiver.garcia96@gmail.com>
2025-11-28 18:59:29 -0500

    feat: add __init__.py to initialize repository modules

---
+++ codigo_refactorizado/repositories/__init__.py
@@ -0,0 +1,4 @@
+from .base_repository import BaseRepository
+from .product_repository import ProductRepository
+from .category_repository import CategoryRepository
+from .favorite_repository import FavoriteRepository
